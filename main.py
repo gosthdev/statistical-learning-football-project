d004ef2237f7155dfae3cbea2b5573aa2e8fa610
Eddu <azabacheeduardo85@gmail.com>
2025-06-15 14:41:55 -0500

    fix: correct class name from API to Api for consistency

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import webview
 import os
-from app.api import API
+from app.api import Api
+
 gui_dir = os.path.join('app', 'gui', 'templates')
 
 if __name__ == '__main__':
-    api = API()
+    api = Api()
 
     # Crea la ventana usando una ruta relativa.
     # pywebview iniciará automáticamente su servidor interno.
