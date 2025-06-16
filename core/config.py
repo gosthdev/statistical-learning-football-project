ebd247733e7ad3aabe40217a643d33c0fab7a759
Eddu <azabacheeduardo85@gmail.com>
2025-06-15 19:54:04 -0500

    feat: add streak and average goals columns to configuration

--- core/config.py
+++ core/config.py
@@ -3,3 +3,5 @@ HOME_TARGET = 'FTHG'
 AWAY_TARGET = 'FTAG'
 N = 5
 ESSENTIAL_COLUMNS = ['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG']
+STREAK_COLUMNS = ['H_WinStreak', 'A_WinStreak']
+AVG_GOALS_COLUMNS = ['H_AvgGoals', 'H_AvgGoalsAgainst', 'A_AvgGoals', 'A_AvgGoalsAgainst']
