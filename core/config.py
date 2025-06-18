aa364a772cc1f78a82741a0f02133c3c9b48294f
Eddu <azabacheeduardo85@gmail.com>
2025-06-18 17:58:07 -0500

    fix:  add FEATURES_COLUMNS in config file

--- core/config.py
+++ core/config.py
@@ -1,4 +1,3 @@
-FEATURES = []
 HOME_TARGET = 'FTHG'
 AWAY_TARGET = 'FTAG'
 N = 5
@@ -9,3 +8,6 @@ AVG_SHOTS_COLUMNS = ['H_AvgShots', 'H_AvgShotsAgainst', 'A_AvgShots', 'A_AvgShot
 AVG_CORNERS_COLUMNS = ['H_AvgCorners', 'H_AvgCornersAgainst', 'A_AvgCorners', 'A_AvgCornersAgainst']
 AVG_POINTS_COLUMNS = ['H_Points', 'A_Points']
 EFFICIENCY_COLUMNS = ['H_Eff_GoalsPerShot', 'A_Eff_GoalsPerShot']
+FEATURES_COLUMNS = STREAK_COLUMNS + AVG_GOALS_COLUMNS + AVG_SHOTS_COLUMNS + AVG_CORNERS_COLUMNS + AVG_POINTS_COLUMNS + EFFICIENCY_COLUMNS
+
+N_SPLITS = 10
