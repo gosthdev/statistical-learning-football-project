5d21e2b74dc87799f6e594b775cbc8ceb8a5ac31
Eddu <azabacheeduardo85@gmail.com>
2025-06-18 18:00:10 -0500

    refactor: update window title and clean up comments in main.py

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ gui_dir = os.path.join('app', 'gui', 'templates')
 if __name__ == '__main__':
     api = Api()
 
-    # Crea la ventana usando una ruta relativa.
-    # pywebview iniciará automáticamente su servidor interno.
     window = webview.create_window(
-        'Cargar Datos Históricos',
-        os.path.join(gui_dir, 'upload.html'), # <- La ruta relativa
+        'Upload Files',
+        os.path.join(gui_dir, 'upload.html'),
         js_api=api,
         min_size=(800, 650)
     )
     
-    # No se necesitan argumentos especiales para el servidor
     webview.start(debug=False)
